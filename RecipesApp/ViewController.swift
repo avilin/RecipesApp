72a786cf6700a69b4149a091a0cddeeee1238beb
Andrés Vicente Linares <andresvicentelinares@gmail.com>
2017-02-18 11:30:04 +0100

    Create project

---
+++ RecipesApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RecipesApp
+//
+//  Created by Andrés Vicente Linares on 18/2/17.
+//  Copyright © 2017 Andrés Vicente Linares. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
