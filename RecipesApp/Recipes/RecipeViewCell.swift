4330482973758fc548e9511d42a3594d0e7050af
Andrés Vicente Linares <andresvicentelinares@gmail.com>
2017-02-19 17:06:29 +0100

    Create and bind RecipeViewController

---
+++ RecipesApp/Recipes/RecipeViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeViewCell.swift
+//  RecipesApp
+//
+//  Created by Andrés Vicente Linares on 19/2/17.
+//  Copyright © 2017 Andrés Vicente Linares. All rights reserved.
+//
+
+import UIKit
+
+class RecipeViewCell: UITableViewCell {
+
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var ingredientesNumberLabel: UILabel!
+
+}
